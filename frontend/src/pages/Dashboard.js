e36ee6269ca2957f89a2033bfe16f65e8f75d7dd
Anton Fleming <aflemingrocks089@gmail.com>
2025-03-24 15:40:11 -0500

    Created a dashboard.js for url search and add fetch code to the getter form

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function Dashboard() {
+  function useFetch(event) {
+    event.preventDefault();
+
+    // TO DO gets url but returns the HTML file
+    fetch(URL, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(),
+    })
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error: ", error));
+  }
+
+  const [url, setUrl] = useState("");
+
   return (
     <>
       <div>
-        <form>
-          <label for="url-search">Url Search</label>
-          <input id="url-search" placeholder="commercebank.com"></input>
-          <button onClick={fetchData()}>Submit</button>
+        <form onSubmit={useFetch}>
+          <label htmlFor="url-search">Url Search</label>
+          <input
+            id="url-search"
+            placeholder="commercebank.com"
+            onChange={(e) => setUrl(e.target.value)}></input>
+          <button type="submit">Fetch</button>
         </form>
       </div>
     </>
